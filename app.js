7f1be657f79864672acc4d25d29823f70c5d8c03
gopal-12336 <goswamivinod2305@gmail.com>
2026-07-25 16:58:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+// App Loaded
+console.log("App Loaded");
 
+// Login Button
+document.getElementById("loginBtn").addEventListener("click", function () {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const message = document.getElementById("message");
+
+    if (email === "" || password === "") {
+        message.innerHTML = "Please enter Email and Password.";
+        return;
+    }
+
+    auth.signInWithEmailAndPassword(email, password)
+
+    .then(function(userCredential){
+
+        localStorage.setItem("userLoggedIn","true");
+        localStorage.setItem("userEmail", email);
+
+        window.location.href = "survey.html";
+
+    })
+
+    .catch(function(error){
+
+        message.innerHTML = error.message;
+
+    });
+
+});
