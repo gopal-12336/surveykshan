40d212e5524492f3669c45e67f3f591c69a4ee30
gopal-12336 <goswamivinod2305@gmail.com>
2026-07-25 16:57:16 +0530

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization code.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
+// Firebase Configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCQVPtZYcLAIk55m9eG_sfXV5awTUDv024",
+  authDomain: "surveykshan-2e4d6.firebaseapp.com",
+  projectId: "surveykshan-2e4d6",
+  storageBucket: "surveykshan-2e4d6.firebasestorage.app",
+  messagingSenderId: "374529268493",
+  appId: "1:374529268493:web:7072c04b92f79b38ecae44"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firebase Services
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+console.log("Firebase Connected Successfully");
