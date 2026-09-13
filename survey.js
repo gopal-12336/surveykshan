260da662d9f94b93b1c46a8e16a2ba2a0daeb0ff
gopal-12336 <goswamivinod2305@gmail.com>
2026-09-13 11:09:14 +0530

    Update survey.js

--- survey.js
+++ survey.js
@@ -376,4 +376,3 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-                                                                                                                   
